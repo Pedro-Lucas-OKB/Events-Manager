20963282109b623f6bcd87198e847416a191e1b7
Pedro Lucas <pedrogmobile2@gmail.com>
2024-06-27 14:04:11 -0300

    feat: adds context conigurations

--- src/Core/EventsManager.Domain/Entities/Event.cs
+++ src/Core/EventsManager.Domain/Entities/Event.cs
@@ -5,12 +5,12 @@ public class Event : BaseEntity
     public string Name { get; set;} = string.Empty;
     public DateTime StartDate { get; set;}
     public DateTime EndDate { get; set;}
-    public string? Location { get; set;} = string.Empty;
-    public string? Category { get; set;} = string.Empty;
-    public string? Description { get; set;} = string.Empty;
+    public string Location { get; set;} = string.Empty;
+    public string Category { get; set;} = string.Empty;
+    public string Description { get; set;} = string.Empty;
     public int ParticipantsLimit { get; set;} = 50;
     public decimal Price { get; set;} = 0;
     public int AdministratorId { get; set;}
-    public Administrator? Administrator { get; set;}
+    public Administrator? Administrator { get; set;} = null!;
     public List<Participant> Participants { get; set;} = [];
 }
---
+++ src/Infrastructure/EventsManager.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using EventsManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsManager.Persistence.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Event> Events { get; set;}
+    public DbSet<Participant> Participants { get; set;}
+    public DbSet<Administrator> Administrators { get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>()
+            .HasOne(e => e.Administrator)
+            .WithMany(a => a.Events)
+            .HasForeignKey(e => e.AdministratorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Event>()
+            .HasMany(e => e.Participants)
+            .WithMany(p => p.Events);
+    }
+}

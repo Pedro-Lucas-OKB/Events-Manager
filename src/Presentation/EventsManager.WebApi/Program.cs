139ff786b1a0b5d0b7133b05f9577eadd9b13f62
Pedro Lucas <pedrogmobile2@gmail.com>
2024-06-27 14:18:33 -0300

    docs: adds basic api documentations

--- src/Presentation/EventsManager.WebApi/Program.cs
+++ src/Presentation/EventsManager.WebApi/Program.cs
@@ -1,5 +1,6 @@
 using EventsManager.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? ""));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x => 
+{
+    x.SwaggerDoc("v1", new OpenApiInfo 
+    {
+        Title = "Events Manager API",
+        Version = "v1",
+        Description = "API para gerenciamento de eventos",
+        Contact = new OpenApiContact
+        {
+            Name = "Pedro Lucas",
+            Email = "pedrogmobile2@gmail.com",
+            Url = new Uri("https://github.com/Pedro-Lucas-OKB")
+        }
+    });
+});
 
 var app = builder.Build();
 
